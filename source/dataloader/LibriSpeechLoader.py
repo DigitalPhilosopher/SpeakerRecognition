58180579858761764db043a12c8e91eec05bd277
Patrick Kühn <pkpatrickkuehn@gmail.com>
2024-06-14 18:08:36 +0200

    Make own loader

--- source/dataloader/LibriSpeechLoader.py
+++ source/dataloader/LibriSpeechLoader.py
@@ -1,26 +1,32 @@
 from .Loader import Loader
 import pandas as pd
-from datasets import load_dataset, concatenate_datasets
+import os
 
 
 class LibriSpeechLoader(Loader):
     def read_data(self, directory):
-        librispeech = []
-        if not directory == []:
-            datasets = []
-            for data in directory:
-                dataset = load_dataset(
-                    'DigitalPhilosopher/librispeech_asr', data["name"], split=data["split"], trust_remote_code=True)
-                datasets.append(dataset)
-            librispeech = concatenate_datasets(datasets)
-        self.data_list = [{
-            'filename': entry['audio']['path'],
-            'utterance': entry['id'],
-            'speaker': entry['speaker_id'],
-            'method_type': 'bona_fide',
-            'method_name': 'bona_fide',
-            'vocoder': 'bona_fide',
-            'is_genuine': 1
-        } for entry in librispeech]
-        self.data_list = pd.DataFrame(self.data_list, columns=[
-                                      'filename', 'utterance', 'speaker', 'method_type', 'method_name', 'vocoder', 'is_genuine',])
+        base = "/mnt/e/LibriSpeech/"  # TODO clean this
+        data_list = []
+        for set in directory:
+            split = set['split'].split(".")
+            dir = base + split[0] + "-" + set['name']
+            if len(split) > 1:
+                dir += "-" + split[1]
+            for root, dirs, files in os.walk(dir):
+                for file in files:
+                    if not file.endswith(".flac"):
+                        continue
+                    utterance = os.path.splitext(
+                        os.path.basename(os.path.join(root, file)))[0]
+                    speaker_id = int(utterance.split("-")[0])
+                    data_list.append({
+                        'filename': os.path.join(root, file),
+                        'utterance': utterance,
+                        'speaker': speaker_id,
+                        'method_type': 'bonafide',
+                        'method_name': 'bonafide',
+                        'vocoder': 'bonafide',
+                        'is_genuine': 1
+                    })
+        self.data_list = pd.DataFrame(data_list, columns=[
+            'filename', 'utterance', 'speaker', 'method_type', 'method_name', 'vocoder', 'is_genuine',])
