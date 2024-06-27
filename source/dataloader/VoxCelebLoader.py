c24a154279347734261be185f8d71141f4ad533e
Patrick Kühn <pkpatrickkuehn@gmail.com>
2024-06-27 14:11:59 +0200

    VoxCelebLoader

--- source/dataloader/VoxCelebLoader.py
+++ source/dataloader/VoxCelebLoader.py
@@ -6,15 +6,12 @@ import os
 class VoxCelebLoader(Loader):
     def read_data(self, directory):
         base = f"{os.getcwd()}/../data/VoxCeleb"
-        data_list = []
+        data_list = pd.DataFrame()
         for set in directory:
             split = set['split']
-            if len(data_list):
-                data_list += pd.read_csv(
-                    f"{base}/{set['name']}/{set['split']}/metadata.csv")
-            else:
-                data_list = pd.read_csv(
-                    f"{base}/{set['name']}/{set['split']}/metadata.csv")
+            current_df = pd.read_csv(
+                f"{base}/{set['name']}/{set['split']}/metadata.csv")
+            data_list = pd.concat([data_list, current_df], ignore_index=True)
 
         self.data_list = pd.DataFrame(data_list, columns=[
             'filename', 'utterance', 'speaker', 'method_type', 'method_name', 'vocoder', 'is_genuine',])
